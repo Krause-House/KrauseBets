911a6a6e1c86b6e8fd724e4c897b8a499c883854
gregrolwes <rolwesg@gmail.com>
2021-12-15 22:02:06 -0600

    Remove stray log statements

--- src/util/dates.js
+++ src/util/dates.js
@@ -1,9 +1,6 @@
 const isWithinHours = (date, hours) => {
   const cutoff = new Date();
   cutoff.setHours(cutoff.getHours() + hours);
-  console.log(cutoff);
-  console.log(date);
-  console.log(date > cutoff);
   return date < cutoff && date > new Date();
 };
 
