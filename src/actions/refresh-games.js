ed7e0f3fd74a83fb009eb3e54d370b4dfc7019ee
gregrolwes <rolwesg@gmail.com>
2022-01-05 22:35:18 -0600

    Remove deprecated refresh-games

--- src/actions/refresh-games.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-const { addGame } = require("../data/games");
-
-axios.get("http://data.nba.net/prod/v1/2021/schedule.json").then((response) => {
-  response.data.league.standard.map(async (game) => {
-    console.log(game);
-    await setTimeout(addGame(game), 100);
-  });
-});
